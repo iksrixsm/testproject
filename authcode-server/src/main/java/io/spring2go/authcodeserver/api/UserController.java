7740d1805d100ff084d5a9c9b68d91ecbe7c87de
IKARI <752539242@QQ.COM>
2019-08-22 12:41:39 +0800

    '333'

--- authcode-server/src/main/java/io/spring2go/authcodeserver/api/UserController.java
+++ authcode-server/src/main/java/io/spring2go/authcodeserver/api/UserController.java
@@ -12,13 +12,13 @@ public class UserController {
 	// 资源API
     @RequestMapping("/api/userinfo")
     public ResponseEntity<UserInfo> getUserInfo() {
-        User user = (User) SecurityContextHolder.getContext()
+        User usrer = (User) SecurityContextHolder.getContext()
                 .getAuthentication().getPrincipal();
         String email = user.getUsername() + "@spring2go.com";
 
         UserInfo userInfo = new UserInfo();
-        userInfo.setName(user.getUsername());
-        userInfo.setEmail(email);
+        userInfo.setNagggme(user.getUsername());
+        userInfo.setggggEmail(email);
 
         return ResponseEntity.ok(userInfo);
     }
